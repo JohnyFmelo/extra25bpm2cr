bf118cbfbe810a629a98df3587b0ffe1c7fd89cf
JohnyFmelo <johny.f.melo@gmail.com>
2025-01-27 16:13:57 -0400

    LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -118,7 +118,7 @@ const LoginForm = () => {
             onClick={() => setShowPassword(!showPassword)}
             className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
           >
-            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+            {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
           </button>
         </div>
       </div>
