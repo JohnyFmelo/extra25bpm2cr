0e67f6512b10acf846d32d31d0548447f58326ea
JohnyFmelo <johny.f.melo@gmail.com>
2025-02-14 15:08:09 -0400

    Update TimeSlotsList.tsx

--- src/components/TimeSlotsList.tsx
+++ src/components/TimeSlotsList.tsx
@@ -176,36 +176,6 @@ const TimeSlotLimitControl = ({
 // Função getMilitaryRankWeight
 // Retorna um peso numérico para cada patente militar para fins de ordenação.
 const getMilitaryRankWeight = (rank: string): number => {
-  // Objeto que mapeia patentes militares para seus respectivos pesos
-  const rankWeights: { [key: string]: number } = {
-    "Cel": 12,
-    "Cel PM": 12,
-    "Ten Cel": 11,
-    "Ten Cel PM": 11,
-    "Maj": 10,
-    "Maj PM": 10,
-    "Cap": 9,
-    "Cap PM": 9,
-    "1° Ten": 8,
-    "1° Ten PM": 8,
-    "2° Ten": 7,
-    "2° Ten PM": 7,
-    "Sub Ten": 6,
-    "Sub Ten PM": 6,
-    "1° Sgt": 5,
-    "1° Sgt PM": 5,
-    "2° Sgt": 4,
-    "2° Sgt PM": 4,
-    "3° Sgt": 3,
-    "3° Sgt PM": 3,
-    "Cb": 2,
-    "Cb PM": 2,
-    "Sd": 1,
-    "Sd PM": 1,
-    "Estágio": 0,
-  };
-  return rankWeights[rank] || 0; // Retorna o peso da patente, ou 0 se a patente não for encontrada
-};
 
 // Componente TimeSlotsList
 // TimeSlotsList exibe a lista de horários disponíveis e lida com ações de voluntariado.
