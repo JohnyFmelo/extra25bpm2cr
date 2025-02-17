8ae4605d669d3cd8e30a0567cbc4bc9e6f329f4c
JohnyFmelo <johny.f.melo@gmail.com>
2025-02-17 17:47:40 -0400

    Update hoursService.js

--- src/services/hoursService.js
+++ src/services/hoursService.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://evsfhznfnifmqlpktbdr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2c2Zoem5mbmlmbXFscGt0bGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwNDY1MjMsImV4cCI6MjA1MjYyMjUyM30.5Khm1eXEsm8SCuN7VYEVRYSbKc0A-T_Xo4hUUvibkgM'; // CHAVE ANON - Verifique se é a ANON KEY correta do seu projeto Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2c2Zoem5mbmlmbXFscGt0YmRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzA0NjUyMywiZXhwIjoyMDUyNjIyNTIzfQ.GQEz9xvQmTr0Chtjj1ndtq3WTAA8YMm_dFPvnAHMLuc'; // CHAVE ANON - Verifique se é a ANON KEY correta do seu projeto Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const getTableName = (month) => {
