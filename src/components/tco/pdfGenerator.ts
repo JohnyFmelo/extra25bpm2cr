8f90a89c165900286f6ef087bafceeee7e4a6014
JohnyFmelo <johny.f.melo@gmail.com>
2025-05-13 23:50:12 -0400

    Update pdfGenerator.ts

--- src/components/tco/pdfGenerator.ts
+++ src/components/tco/pdfGenerator.ts
@@ -16,7 +16,7 @@ import { addTermoApreensao } from './PDF/PDFTermoApreensao.js';
 import { addTermoConstatacaoDroga } from './PDF/PDFTermoConstatacaoDroga.js';
 import { addRequisicaoExameLesao } from './PDF/PDFTermoRequisicaoExameLesao.js';
 import { addTermoEncerramentoRemessa } from './PDF/PDFTermoEncerramentoRemessa.js';
-import { addRequisicaoExameDrogas } from './PDF/PDFRequisicaoExameDrogas.js'; // Corrigido o nome do arquivo
+import { addRequisicaoExameDrogas } from './PDF/PDFpericiadrogas.js'; // Corrigido o nome do arquivo
 
 // Função auxiliar para adicionar imagens ao PDF
 const addImagesToPDF = (doc: jsPDF, yPosition: number, images: { name: string; data: string }[], pageWidth: number, pageHeight: number) => {
